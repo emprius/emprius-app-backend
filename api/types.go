e6837360096b2ede0a267deace4502b67905e054
p4u <pau@dabax.net>
2025-01-27 10:36:47 +0100

    use toolCategory instead of category
    Signed-off-by: p4u <pau@dabax.net>

--- api/types.go
+++ api/types.go
@@ -126,7 +126,7 @@ type Tool struct {
 	Cost             *uint64          `json:"cost"`
 	Images           []types.HexBytes `json:"images"`
 	TransportOptions []int            `json:"transportOptions"`
-	Category         int              `json:"category"`
+	Category         int              `json:"toolCategory"`
 	Location         Location         `json:"location"`
 	EstimatedValue   uint64           `json:"estimatedValue"`
 	Height           uint32           `json:"height"`
