bd1bdbbfe3f6e4c6f777daf84261c3cfbac2146e
p4u <pau@dabax.net>
2025-02-24 01:01:26 +0100

    update ratings migration
    Signed-off-by: p4u <pau@dabax.net>

--- db/migrations.go
+++ db/migrations.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/rs/zerolog/log"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -20,9 +21,9 @@ type Migration struct {
 
 // RunMigrations iterates through all migration functions and applies those not yet applied.
 func RunMigrations(ctx context.Context, db *mongo.Database) error {
-	// List of migration functions. Add new migrations here.
+	// List of migration functions. Disable the old one and add the new migration.
 	migrations := []func(context.Context, *mongo.Database) error{
-		migrateBookingRatings,
+		migrateBookingRatingsToNewCollection, // New migration for the new ratings structure.
 	}
 
 	for i, migration := range migrations {
@@ -67,13 +68,17 @@ func markMigrationApplied(ctx context.Context, db *mongo.Database, migrationID s
 	return err
 }
 
-// migrateBookingRatings migrates legacy rating fields (ratedBy, rating, ratingComment, ratedAt)
-// into the new Ratings array structure.
-func migrateBookingRatings(ctx context.Context, db *mongo.Database) error {
-	collection := db.Collection("bookings")
+// migrateBookingRatingsToNewCollection migrates legacy rating fields stored in the bookings collection
+// into the new ratings collection. For each booking document that has a legacy rating (legacy fields exist
+// as a single rating stored under "ratedBy", etc.), a new rating document is inserted into the ratings collection.
+// The new document uses BookingID = booking._id, and determines the counterparty (ToUserID) based on whether
+// the legacy ratedBy equals booking.FromUserID or booking.ToUserID. Then the legacy fields are removed.
+func migrateBookingRatingsToNewCollection(ctx context.Context, db *mongo.Database) error {
+	bookingsColl := db.Collection("bookings")
+	ratingsColl := db.Collection("ratings")
 
-	// Select documents where the legacy "ratedBy" field exists as an ObjectID (BSON type 7),
-	// which excludes documents already migrated (where ratedBy is an array).
+	// Select documents where the legacy "ratedBy" field exists as an ObjectID (BSON type 7)
+	// (thus not already migrated).
 	filter := bson.M{
 		"ratedBy": bson.M{
 			"$exists": true,
@@ -81,11 +86,11 @@ func migrateBookingRatings(ctx context.Context, db *mongo.Database) error {
 		},
 	}
 
-	cursor, err := collection.Find(ctx, filter)
+	cursor, err := bookingsColl.Find(ctx, filter)
 	if err != nil {
 		return err
 	}
-	defer cursor.Close(ctx) // nolint: errcheck
+	defer cursor.Close(ctx)
 
 	for cursor.Next(ctx) {
 		var booking bson.M
@@ -93,36 +98,71 @@ func migrateBookingRatings(ctx context.Context, db *mongo.Database) error {
 			return err
 		}
 
-		var ratings []bson.M
-		if ratedBy, ok := booking["ratedBy"]; ok {
-			ratingDoc := bson.M{
-				"userId": ratedBy,
-			}
-			if rating, ok := booking["rating"]; ok {
-				ratingDoc["rating"] = rating
-			}
-			if ratingComment, ok := booking["ratingComment"]; ok {
-				ratingDoc["ratingComment"] = ratingComment
-			}
-			if ratedAt, ok := booking["ratedAt"]; ok {
-				ratingDoc["ratedAt"] = ratedAt
-			}
-			ratings = append(ratings, ratingDoc)
+		bookingID, ok := booking["_id"].(primitive.ObjectID)
+		if !ok {
+			continue
 		}
 
+		legacyRatedBy, ok := booking["ratedBy"].(primitive.ObjectID)
+		if !ok {
+			continue
+		}
+
+		// Extract rating value from legacy "rating" field.
+		legacyRating, ok := booking["rating"]
+		if !ok {
+			continue
+		}
+		var ratingValue int
+		switch v := legacyRating.(type) {
+		case int32:
+			ratingValue = int(v)
+		case int64:
+			ratingValue = int(v)
+		case float64:
+			ratingValue = int(v)
+		default:
+			continue
+		}
+
+		legacyRatingComment, _ := booking["ratingComment"].(string)
+		legacyRatedAt, _ := booking["ratedAt"].(time.Time)
+
+		// Get booking's fromUserId and toUserId.
+		fromUserID, _ := booking["fromUserId"].(primitive.ObjectID)
+		toUserID, _ := booking["toUserId"].(primitive.ObjectID)
+		var cp primitive.ObjectID
+		if legacyRatedBy == fromUserID {
+			cp = toUserID
+		} else {
+			cp = fromUserID
+		}
+
+		newRating := bson.M{
+			"bookingId":     bookingID,
+			"fromUserId":    legacyRatedBy,
+			"toUserId":      cp,
+			"rating":        ratingValue,
+			"ratingComment": legacyRatingComment,
+			"ratedAt":       legacyRatedAt,
+		}
+		_, err := ratingsColl.InsertOne(ctx, newRating)
+		if err != nil {
+			return err
+		}
+
+		// Remove the legacy fields from the booking.
 		update := bson.M{
-			"$set": bson.M{
-				"ratings": ratings,
-			},
 			"$unset": bson.M{
 				"ratedBy":       "",
 				"rating":        "",
 				"ratingComment": "",
 				"ratedAt":       "",
+				"ratings":       "", // In case a legacy ratings array exists.
 			},
 		}
-
-		if _, err := collection.UpdateOne(ctx, bson.M{"_id": booking["_id"]}, update); err != nil {
+		_, err = bookingsColl.UpdateOne(ctx, bson.M{"_id": bookingID}, update)
+		if err != nil {
 			return err
 		}
 	}
